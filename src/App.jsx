a8620c4fa7689b62cf27de0a99e60908064a7969
kevin <kevingitabr@gmail.com>
2024-05-07 19:53:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import { fetchData } from './utils/api'
+
+function App() {
+
+
+  useEffect(() => {
+    apiTest();
+  }, [])
+
+  const apiTest=async()=>{
+    fetchData('/movie/popular').
+    then((res)=>console.log(res))
+
+  }
+  
+  return (
+    <>
+      App
+    </>
+  )
+}
+
+export default App
